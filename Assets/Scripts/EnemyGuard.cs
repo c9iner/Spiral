9fffa8060da26e3bd4ec0a09e0885beaa3462257
Chris <chris.wheeler@emergentvr.com>
2018-10-23 22:13:42 -0700

    Add gravity flip, enemy guard, and death particles

---
+++ Assets/Scripts/EnemyGuard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGuard : MonoBehaviour {
+
+    public float patrolDistance = 3;
+    public float patrolSpeed = 3;
+    public float patrolPhase = 0;
+    public GameObject body;
+
+    private Vector3 _startPosition;
+
+	// Use this for initialization
+	void Awake () {
+        _startPosition = body.transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var xOffset = Mathf.Sin(Time.fixedTime * patrolSpeed) / 2 * patrolDistance + patrolPhase;
+        body.transform.localPosition = new Vector3(_startPosition.x + xOffset, _startPosition.y, _startPosition.z);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public float acceleration = 100;
     public float deceleration = 0.9f;
@@ -11,29 +11,32 @@ public class PlayerMovement : MonoBehaviour {
     public float jump = 500;
     public int maxJumps = 2;
     public GameObject gravityCenter;
-    public GameObject goal;
     public float gravityStrength = 20;
     public float furryness = 5.5f;
     public float eyeSize = 3.2f;
     public int numEyes = 3;
     public Vector3 rotationOffset;
+    public GameObject dieFX;
 
     private Vector3 _startPosition;
     private Rigidbody _rigidBody;
     private int _remainingJumps;
     private Vector3 _gravityVector;
+    private float _gravityDirection = -1;
+    private bool _isTouchingGround = false;
+    private bool _isDying = false;
 
-    void Start ()
+    void Awake ()
     {
         _startPosition = transform.position;
         _rigidBody = GetComponent<Rigidbody>();
+        _remainingJumps = maxJumps;
     }
 
     void Update()
     {
-        
         // Gravity
-        _gravityVector = (transform.position - gravityCenter.transform.position).normalized;
+        _gravityVector = _gravityDirection * (transform.position - gravityCenter.transform.position).normalized;
         Physics.gravity = _gravityVector * gravityStrength;
         Debug.DrawRay(gravityCenter.transform.position, Physics.gravity * 10, Color.blue);
 
@@ -45,8 +48,14 @@ public class PlayerMovement : MonoBehaviour {
             transform.Rotate(new Vector3(0, 0, axis), alignAngle);
         }
 
+        // Quit
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         // Reset
-        if (Input.GetKey(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && !_isDying)
         {
             Reset();
         }
@@ -74,7 +83,7 @@ public class PlayerMovement : MonoBehaviour {
 
         // Calculate new velocity
         // Max speed and deceleration are only applied in the direction perpendicular to gravity
-        var playerForward = transform.right * axis;
+        var playerForward = transform.right * axis * _gravityDirection;
         var velocityForward = playerForward * Vector3.Dot(playerForward, _rigidBody.velocity);
         if (velocityForward.sqrMagnitude > maxSpeed * maxSpeed)
             velocityForward = playerForward * maxSpeed;
@@ -87,18 +96,57 @@ public class PlayerMovement : MonoBehaviour {
 
     void OnCollisionEnter(Collision col)
     {
+        _isTouchingGround = true;
         _remainingJumps = maxJumps;
 
         if (col.gameObject.name == "Goal")
-        {
             Reset();
+        else if (col.gameObject.GetComponentInParent<EnemyGuard>())
+            StartCoroutine(Die());
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "GravityFlip")
+        {
+            _gravityDirection *= -1;
         }
     }
 
-    void Reset()
+    void OnCollision(Collision col)
     {
+        _isTouchingGround = true;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        _isTouchingGround = false;
+    }
+
+    private void Reset()
+    {
+        _rigidBody.isKinematic = false;
+        GetComponent<MeshRenderer>().enabled = true;
         transform.position = _startPosition;
         transform.rotation = Quaternion.identity;
         _rigidBody.velocity = Vector3.zero;
+        _gravityDirection = -1;
+    }
+
+    private IEnumerator Die()
+    {
+        _isDying = true;
+
+        GetComponent<MeshRenderer>().enabled = false;
+        _rigidBody.isKinematic = true;
+
+        // Spawn fx
+        var fx = Instantiate(dieFX, transform);
+        yield return new WaitForSecondsRealtime(5);
+        Destroy(fx);
+
+        _isDying = false;
+
+        Reset();
     }
 }
