5b1cd8ddb47d5da08351a2dea53b20984221f2b9
Chris <chris.wheeler@emergentvr.com>
2018-11-17 10:03:36 -0800

    Constrain to parent, organize scripts

---
+++ Assets/Scripts/ConstrainToParent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstrainToParent : MonoBehaviour {
+
+    public Transform parent;
+
+    private Rigidbody _rigidBody;
+    private Vector3 _positionStart;
+    private Vector3 _lastParentPosition;
+
+	// Use this for initialization
+	void Start () {
+        _rigidBody = GetComponent<Rigidbody>();
+        _lastParentPosition = parent.position;
+        _positionStart = transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = parent.TransformPoint(_positionStart);
+        _rigidBody.AddForce((_lastParentPosition - parent.position)*1000);
+        _lastParentPosition = parent.position;
+    }
+}
