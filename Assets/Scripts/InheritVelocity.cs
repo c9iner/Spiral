5f493148fb0b0507908b37fe7dbe02bbfa8604c6
Chris <chris.wheeler@emergentvr.com>
2018-11-17 10:23:12 -0800

    Antenna inherits velocity from body

---
+++ Assets/Scripts/InheritVelocity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritVelocity : MonoBehaviour {
+
+    public Transform parent;
+
+    private Rigidbody _rigidBody;
+    private Vector3 _lastParentPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _lastParentPosition = parent.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidBody.AddForce((_lastParentPosition - parent.position) * 1000);
+        _lastParentPosition = parent.position;
+    }
+}
