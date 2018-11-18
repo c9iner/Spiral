26db335ac7bafca56bd3b8bbf017a27a0a9141ec
Chris <chris.wheeler@emergentvr.com>
2018-11-17 17:48:19 -0800

    Rabbit character. Leg jumping animation

--- Assets/Scripts/ConstrainToParent.cs
+++ Assets/Scripts/ConstrainToParent.cs
@@ -6,12 +6,10 @@ public class ConstrainToParent : MonoBehaviour {
 
     public Transform parent;
 
-    private Rigidbody _rigidBody;
     private Vector3 _positionStart;
 
 	// Use this for initialization
 	void Start () {
-        _rigidBody = GetComponent<Rigidbody>();
         _positionStart = transform.localPosition;
     }
 	
