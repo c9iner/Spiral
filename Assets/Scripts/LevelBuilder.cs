bed1d773e66b23368668dcc8a37de63ff17297ed
Chris <chris.wheeler@emergentvr.com>
2018-11-21 09:57:04 -0800

    Level builder updates - stacked sin waves

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -9,10 +9,33 @@ using UnityEngine;
 public class LevelBuilder : MonoBehaviour {
 
     public float width = 10;
-    public float height = 10;
+    public float depth = 10;
+    public float radius = 100;
     public int numPanels = 8;
+    public float angleRangeMin = -2;
+    public float angleRangeMax = 2;
+    public float speed = 1;
+    public float acceleration = 0.1f;
+    public float waveFrequency1 = 0.01f;
+    public float waveScale1 = 0.03f;
+    public float waveFrequency2 = 0.01f;
+    public float waveScale2 = 0.02f;
 
-	void Start ()
+    private void Start()
+    {
+        UpdateMesh();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            UpdateMesh();
+
+        speed += acceleration;
+        transform.Translate(0,0,Time.deltaTime * -speed);
+    }
+
+    void UpdateMesh()
     {
         var mesh = new Mesh();
 
@@ -32,10 +55,27 @@ public class LevelBuilder : MonoBehaviour {
         normals[0] = -Vector3.forward;
         normals[1] = -Vector3.forward;
 
+        float angle = 0;
+        float xRotated = 0;
+        float yRotated = 0;
+        float sinTheta = 0;
+        float cosTheta = 0;
         for (int i=2, j=0; i < numPanels+2; i+=2, j+=6)
         {
-            vertices[i] = new Vector3(0, 0, i * height);
-            vertices[i+1] = new Vector3(width, 0, i * height);
+            var path = Mathf.Sin(i * depth * waveFrequency1) * Random.Range(waveScale1, waveScale2);
+            path += Mathf.Sin(i * depth * waveFrequency2) * Random.Range(waveScale1, waveScale2);
+            angle = path + Mathf.Deg2Rad * Random.Range(angleRangeMin, angleRangeMax);
+
+            sinTheta = Mathf.Sin(angle);
+            cosTheta = Mathf.Cos(angle);
+
+            xRotated = -sinTheta * -radius;
+            yRotated = cosTheta * -radius;
+            vertices[i] = new Vector3(xRotated, yRotated + radius, i * depth);
+
+            xRotated = (width * cosTheta) - (-radius * sinTheta);
+            yRotated = (width * sinTheta) + (-radius * cosTheta);
+            vertices[i+1] = new Vector3(xRotated, yRotated + radius, i * depth);
 
             normals[i] = -Vector3.forward;
             normals[i + 1] = -Vector3.forward;
@@ -66,7 +106,4 @@ public class LevelBuilder : MonoBehaviour {
         //meshCollider.sharedMesh = mesh;
     }
 
-	void Update () {
-		
-	}
 }
