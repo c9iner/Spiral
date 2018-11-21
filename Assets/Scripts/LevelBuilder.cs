4c6cf1b81605bae0ad08b90cc802612e0581436c
Chris <chris.wheeler@emergentvr.com>
2018-11-20 18:56:42 -0800

    Build triangle strip

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -10,6 +10,7 @@ public class LevelBuilder : MonoBehaviour {
 
     public float width = 10;
     public float height = 10;
+    public int numPanels = 8;
 
 	void Start ()
     {
@@ -20,40 +21,38 @@ public class LevelBuilder : MonoBehaviour {
 
         var meshCollider = GetComponent<MeshCollider>();
         meshCollider.sharedMesh = meshFilter.sharedMesh;
-        
 
-        var vertices = new Vector3[16];
-
-        //for (int i=0; i < 8; ++i)
+        var numVertices = (numPanels * 2) + 2;
+        var vertices = new Vector3[numVertices];
+        var tri = new int[numPanels * 6];
+        var normals = new Vector3[numVertices];
 
         vertices[0] = new Vector3(0, 0, 0);
         vertices[1] = new Vector3(width, 0, 0);
-        vertices[2] = new Vector3(0, 0, height);
-        vertices[3] = new Vector3(width, 0, height);
-
-        mesh.vertices = vertices;
-
-        var tri = new int[6];
-
-        tri[0] = 0;
-        tri[1] = 2;
-        tri[2] = 1;
-
-        tri[3] = 2;
-        tri[4] = 3;
-        tri[5] = 1;
-
-        mesh.triangles = tri;
-
-        var normals = new Vector3[4];
-
         normals[0] = -Vector3.forward;
         normals[1] = -Vector3.forward;
-        normals[2] = -Vector3.forward;
-        normals[3] = -Vector3.forward;
 
+        for (int i=2, j=0; i < numPanels+2; i+=2, j+=6)
+        {
+            vertices[i] = new Vector3(0, 0, i * height);
+            vertices[i+1] = new Vector3(width, 0, i * height);
+
+            normals[i] = -Vector3.forward;
+            normals[i + 1] = -Vector3.forward;
+
+            tri[j + 0] = i - 2;
+            tri[j + 1] = i;
+            tri[j + 2] = i - 1;
+            tri[j + 3] = i;
+            tri[j + 4] = i + 1;
+            tri[j + 5] = i - 1;
+        }
+
+        mesh.vertices = vertices;
         mesh.normals = normals;
+        mesh.triangles = tri;
 
+        /*
         var uv = new Vector2[4];
 
         uv[0] = new Vector2(0, 0);
@@ -62,6 +61,7 @@ public class LevelBuilder : MonoBehaviour {
         uv[3] = new Vector2(1, 1);
 
         mesh.uv = uv;
+        */
 
         //meshCollider.sharedMesh = mesh;
     }
